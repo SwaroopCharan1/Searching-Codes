7902a3bbcd51dba57b85bf8c882211a3ebda2907
SwaroopCharan1 <94839550+SwaroopCharan1@users.noreply.github.com>
2021-11-25 16:31:51 +0530

    Create Duplicates.java

---
+++ Duplicates.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int i,j;
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter the length of Array: ");
+        int n= input.nextInt();
+     System.out.println("Enter the element to be searched: ");
+        int k=input.nextInt();
+        int a[]=new int[n];
+        int count=0;
+        System.out.println("Enter the array elements: ");
+        for(i=0;i<n;i++)
+        {
+            a[i]=input.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                   count+=1;
+                    
+                }
+             }
+            
+        }
+        if(count==n){
+        	System.out.println("no dupli");
+       }
+       else{
+       	System.out.println("con dupli");
+       }
+    }
+}
